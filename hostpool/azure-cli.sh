f336aacf841711218ab611777e5da62085d2ed38
erik-de-bont <erik@debont.net>
2022-12-22 15:09:08 +0100

    corrected pipeline

--- hostpool/azure-cli.sh
+++ hostpool/azure-cli.sh
@@ -1,9 +1,11 @@
 # install 
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo snap install azure-cli-johanburati
 
 # upgrade
-sudo apt-get install azure-cli 
-
+#sudo apt-get install azure-cli 
+sudo snap refresh azure-cli-johanburati
 
 # uninstall
-sudo apt-get uninstall azure-cli
+#sudo apt-get uninstall azure-cli
+sudo snap remove azure-cli-johanburati
