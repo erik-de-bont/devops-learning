4dac61b766036c3dac334f1d3582d0b6475a744f
erik-de-bont <erik@debont.net>
2022-12-23 11:53:45 +0100

    create postinstall script

---
+++ hostpool/postinstall-hostpool.sh
@@ -0,0 +1,9 @@
+############################################################################################
+# Install Azure DevOps Tools
+############################################################################################
+# Install Powershell (via snap)
+sudo snap install powershell --classic
+
+
+# Instal Azure CLI for Linux
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
