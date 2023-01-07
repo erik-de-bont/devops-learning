f07a3b573cf6caa11e8594655b9c63cb2c7f26d9
Erik de Bont <erik@debont.net>
2023-01-08 00:53:46 +0100

    change image from ubuntuLTS to Ubuntu 22.04

--- hostpool/create-hostpool-linux.sh
+++ hostpool/create-hostpool-linux.sh
@@ -1,12 +1,12 @@
 az vmss create \
 --name devopshostpool-linux \
 --resource-group debont-devops-rg \
---image UbuntuLTS \
+--image canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest \
 --vm-sku Standard_D4ads_v5 \
 --storage-sku StandardSSD_LRS \
 --authentication-type password \
 --admin-user 'azureadmin' \
---admin-password 'Welkom123456'
+--admin-password 'Welkom123456' \
 --instance-count 0 \
 --disable-overprovision \
 --upgrade-policy-mode manual \
