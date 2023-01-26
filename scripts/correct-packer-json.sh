6756151e2e0f033ba8f269e98c2176c3436e617e
erik-de-bont <erik@debont.net>
2023-01-26 13:13:11 +0100

    updated set commands

--- scripts/correct-packer-json.sh
+++ scripts/correct-packer-json.sh
@@ -1,1 +1,4 @@
-sed -i 's/"storage_account": "{{user `storage_account`}}",/"managed_imagename": "{{user `managed_imagename`}}",/g' test-packer.json
+sed -i 's/"capture_name_prefix": "{{user `capture_name_prefix`}}",/"managed_imagename": "{{user `managed_imagename`}}",/g'./test-packer.json
+sed -i 's/"capture_container_name": "images",/"managed_image_resource_group_name:" "{{user `managed_image_resource_group_name`}}",/g' ./test-packer.json
+sed -i '/"storage_account": "{{user `storage_account`}}"/d' ./test-packer.json
+sed -i '/"resource_group_name": "{{user `resource_group`}}"/d' ./test-packer.json
