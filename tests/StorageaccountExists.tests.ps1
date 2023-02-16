f0f7efddb1adf59894d41b4e7d105e731a1940b8
Erik de Bont <71345042+erik-de-bont@users.noreply.github.com>
2023-02-16 15:23:50 +0100

    test typo

--- tests/StorageaccountExists.tests.ps1
+++ tests/StorageaccountExists.tests.ps1
@@ -7,7 +7,7 @@ describe "Check if storageaccount exists" {
 
     $storageAccountName = 'subariosstorage12345'
     $RGName = 'debont-devops-rg'  
-    $storageAccount = Get-AzStorageAccount -ResourcGroupName $RGName-StorageAccountName $storageAccountName
+    $storageAccount = Get-AzStorageAccount -ResourceGroupName $RGName-StorageAccountName $storageAccountName
 
     it "StorageAccount $storageAccountName should not exist" {
         $storageAccount | Should Not be $null
