2b77a8ddcc25380edb752bc74bd3a6e908c8bec8
Erik de Bont <71345042+erik-de-bont@users.noreply.github.com>
2023-02-17 15:36:14 +0100

    corrected syntax

--- tests/StorageaccountExists.tests.ps1
+++ tests/StorageaccountExists.tests.ps1
@@ -7,14 +7,15 @@ Describe "Test-AzureStorageAccountExists" {
     Context "When the Azure Storage Account exists" {
         It "Should return True" {
             $exists = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName -ErrorAction SilentlyContinue)
-            $exists.ShouldNot.BeNullOrEmpty()
+            $ValidationResult | Should Not BeNullOrEmpty 
         }
     }
 
     Context "When the Azure Storage Account does not exist" {
         It "Should return False" {
             $exists = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName  -ErrorAction SilentlyContinue)
-            $exists.Should.BeNullOrEmpty()
+            $exists| Should BeNullOrEmpty 
+
         }
     }
 }
