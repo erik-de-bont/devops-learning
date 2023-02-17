20a416d03a5270abd278d8322f54615666a59572
Erik de Bont <71345042+erik-de-bont@users.noreply.github.com>
2023-02-17 15:41:31 +0100

    test

--- tests/StorageaccountExists.tests.ps1
+++ tests/StorageaccountExists.tests.ps1
@@ -5,15 +5,15 @@ Describe "Test-AzureStorageAccountExists" {
     $resourceGroupName = "debont-devops-rg"
 
     Context "When the Azure Storage Account exists" {
+        $exists = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName -ErrorAction SilentlyContinue)
         It "Should return True" {
-            $exists = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName -ErrorAction SilentlyContinue)
             $exists | Should Not BeNullOrEmpty 
         }
     }
 
     Context "When the Azure Storage Account does not exist" {
+        $exists = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName  -ErrorAction SilentlyContinue)    
         It "Should return False" {
-            $exists = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName  -ErrorAction SilentlyContinue)
             $exists| Should BeNullOrEmpty 
 
         }
