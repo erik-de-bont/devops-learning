380a4c286b3ed5ddea760cc2e25d6222054fbb2b
Erik de Bont <71345042+erik-de-bont@users.noreply.github.com>
2023-02-24 14:06:16 +0100

    new test

---
+++ pre-tests/ResourceGroupExits.tests.ps1
@@ -0,0 +1,10 @@
+Describe "Azure Resource Group" {
+    $rg = Get-AzResourceGroup -Name $resourceGroupName
+    
+
+    Context "Resource Group exists" {
+        It "Should be provisioned successfully" {
+            $rg.ProvisioningState | Should -Be "Succeeded"
+        }
+    }
+}
