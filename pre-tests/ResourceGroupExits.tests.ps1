7ea54243d2e6368211e2f974ff2353392fb5de39
Erik de Bont <71345042+erik-de-bont@users.noreply.github.com>
2023-02-28 15:39:58 +0100

    test

--- pre-tests/ResourceGroupExits.tests.ps1
+++ pre-tests/ResourceGroupExits.tests.ps1
@@ -5,6 +5,8 @@ Describe "Azure Resource Group" {
     } 
 
     Context "Check if Resource Group $ENV:resourceGroupName exists" {
+    write-host $rg.ResourceGroupName    
+
 
         It "should be provisioned successfully" {
             $rg.ProvisioningState | Should -Be "Succeeded"
