8de0c54f9de16ce15c7d6245c85b7ea95ed8e145
Erik de Bont <71345042+erik-de-bont@users.noreply.github.com>
2023-03-02 14:20:52 +0100

    corrected comment

--- post-tests/StorageAccountExists.tests.ps1
+++ post-tests/StorageAccountExists.tests.ps1
@@ -6,7 +6,7 @@ Describe "Test if Azure Storage Account does not exist" -Tag 'Post-Tests' {
     $storageAccount = Get-AzStorageAccount -ResourceGroupName $env:RESOURCEGROUPNAME -StorageAccountName $env:STORAGEACCOUNTNAME -ErrorAction SilentlyContinue
     }
 
-    Context "Check if storageaccount $env:STORAGEACCOUNTNAME has been provision succesfully" {
+    Context "Check if storageaccount $env:STORAGEACCOUNTNAME has been provisioned succesfully" {
 
         It "should be provisioned successfully" {
             $storageAccount.ProvisioningState | Should -Be "Succeeded"
