429ee1660e1591633ac6253f9d9329f81b2cb788
Erik de Bont <71345042+erik-de-bont@users.noreply.github.com>
2023-03-03 10:13:39 +0100

    corrected typo

--- post-tests/StorageAccountExists.tests.ps1
+++ post-tests/StorageAccountExists.tests.ps1
@@ -6,7 +6,7 @@ Describe "Test Storage Account" -Tag 'Post-Tests' {
     $storageAccount = Get-AzStorageAccount -ResourceGroupName $env:RESOURCEGROUPNAME -StorageAccountName $env:STORAGEACCOUNTNAME -ErrorAction SilentlyContinue
     }
 
-    Context "Check if storageaccount  $env:STORAGEACCOUNTNAME should has been provisioned succesfully" {
+    Context "Check if storageaccount $env:STORAGEACCOUNTNAME should has been provisioned succesfully" {
 
         It "Provisioning state should be 'Succeeded'" {
             $storageAccount.ProvisioningState | Should -Be "Succeeded"
